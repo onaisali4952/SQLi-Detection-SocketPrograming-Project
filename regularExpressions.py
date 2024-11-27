22c7b508534ff77ecda4b8038d0d2c2418a49166
Syed Onais Ali Shah <onaisali4952@gmail.com>
2024-11-27 20:00:14 +0500

    RegEx fixed

--- regularExpressions.py
+++ regularExpressions.py
@@ -2,18 +2,18 @@ import re
 
 # List of SQL injection patterns
 sql_injection_patterns = [
-    r"union\s+select",  # UNION SELECT
-    r"or\s+1=1",        # OR 1=1
-    r"order\s+by",      # ORDER BY
-    r"--",              # SQL comment
-    r"drop\s+table",    # DROP TABLE
-    r"insert\s+into",   # INSERT INTO
-    r"select\s+\*",     # SELECT *
-    r"\'\s+or",         # ' OR
-    r"\bwhere\b.*\b(or|and)\b.*="   # WHERE statement
-    r"\bupdate\b.*\bset\b.*(\bwhere\b.*(like|or|and|=))"  # UPDATE statement
-    r";.*\b(drop|select|insert|delete|update)\b"  # Multiline statement
-    r"\blike\b.*['\"].*['\"]"  # Catches any LIKE keyword in input
+    r"union\s+select",                              # UNION SELECT
+    r"or\s+1=1",                                   # OR 1=1
+    r"order\s+by",                                 # ORDER BY
+    r"--",                                         # SQL comment
+    r"drop\s+table",                               # DROP TABLE
+    r"insert\s+into",                              # INSERT INTO
+    r"select\s+\*",                                # SELECT *
+    r"\'\s+or",                                    # ' OR
+    r"\bwhere\b.*\b(or|and)\b.*=",                 # WHERE statement
+    r"\bupdate\b.*\bset\b.*(\bwhere\b.*(like|or|and|=))",  # UPDATE statement
+    r";.*\b(drop|select|insert|delete|update)\b",  # Multiline statement
+    r"\blike\b.*['\"].*['\"]"                      # LIKE clause
 ]
 
 # Function to detect SQL injection
